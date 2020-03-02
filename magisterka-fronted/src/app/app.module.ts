2d20404dd2e76aac203744ca6669e90a016bdaea
Maciej Czepan <maciej.czepan@gmail.com>
2020-03-02 15:08:44 +0100

    basketball detail method in basketball controller

--- magisterka-fronted/src/app/app.module.ts
+++ magisterka-fronted/src/app/app.module.ts
@@ -5,15 +5,24 @@ import {AppComponent} from './app.component';
 import {BasketballResultComponent} from "./components/live/basketball/general/basketball-result/basketball-result.component";
 import {HttpClientModule} from "@angular/common/http";
 import {BaskteballService} from "./services/live/basketball/baskteball.service";
-import { NavbarComponent } from './components/live/basketball/navbar/navbar.component';
+import {BasketballNavbarComponent} from './components/live/basketball/navbar/basketball-navbar.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'live/:sport', component: BasketballResultComponent},
+  {path: 'live', component: BasketballResultComponent},
+  {path: '', redirectTo: 'live', pathMatch: 'full'},
+  {path: '**', redirectTo: 'live', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     BasketballResultComponent,
-    NavbarComponent
+    BasketballNavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
